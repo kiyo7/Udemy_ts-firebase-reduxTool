538c105c45c8042934845f9b371002a998462522
kiyo7 <kiyo324783@icloud.com>
2021-12-06 22:48:50 +0900

    tweet機能 firestoreと繋ぐ

--- twitter_app_lesson/src/components/Feed.tsx
+++ twitter_app_lesson/src/components/Feed.tsx
@@ -2,10 +2,17 @@
 import React from 'react';
 import { auth } from '../firebase';
 
+//components
+import { TweetInput } from './TweetInput';
+
+//style
+import styles from './Feed.module.css';
+
 const Feed = () => {
   return (
-    <div>
+    <div className={styles.feed}>
       Feed
+      <TweetInput />
       <button onClick={() => auth.signOut()}>Logout</button>
     </div>
   );
